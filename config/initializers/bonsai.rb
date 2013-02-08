b45ff81fee2221b3073a4432e65d9813fe120ea8
jubari <mail@jubari.com>
2013-02-08 13:07:36 +0100

    bonsai fix

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,8 +1,8 @@
 if Rails.env.production?
   ENV['ELASTICSEARCH_URL'] = ENV['BONSAI_URL']
-  Tire.configure do
-    url "http://index.bonsai.io"
-  end
+  # Tire.configure do
+  #   url "http://index.bonsai.io"
+  # end
 else
   Tire.configure do 
     url "http://127.0.0.1:9200"
