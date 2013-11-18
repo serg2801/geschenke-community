72448049dc2e658cd13854c46d388471ba44e180
jubari <mail@jubari.com>
2013-11-18 09:08:36 +0100

    activeadmin change

---
+++ app/admin/users.rb
@@ -0,0 +1,15 @@
+ActiveAdmin.register User do     
+
+  index do    
+    column :name                        
+    column :email                     
+    column :current_sign_in_at        
+    column :last_sign_in_at           
+    column :sign_in_count             
+    default_actions                   
+  end                                 
+
+  filter :email   
+  filter :name                    
+                             
+end                                   
