9548fd59d79a411d170e45ba5bfa5b6ce38f474c
david <geldern@novastorm.de>
2015-09-21 17:25:44 +0200

    updated hero form

--- app/admin/heroes.rb
+++ app/admin/heroes.rb
@@ -5,8 +5,8 @@ ActiveAdmin.register_page "Helden" do
   content :title => "Helden" do
     div :class => "blank_slate_container", :id => "dashboard_default_message" do
       form_tag("/heroes/add", :method => "post") do
-        text_field_tag(:hero_name)
-        text_area(:post, :description )
+        text_field_tag 'hero_name'
+        text_area 'post', 'description'
         submit_tag("Add Hero", :class => "btn ")
       end
     end
