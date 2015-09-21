7760715a652fab0d6045ca7702c192b5c9ee6abc
david <geldern@novastorm.de>
2015-09-21 17:16:08 +0200

    added textarea for hero description

--- app/admin/heroes.rb
+++ app/admin/heroes.rb
@@ -5,8 +5,9 @@ ActiveAdmin.register_page "Helden" do
   content :title => "Helden" do
     div :class => "blank_slate_container", :id => "dashboard_default_message" do
       form_tag("/heroes/add", :method => "post") do
-        text_field_tag(:heroe_name)
-        submit_tag("Add Heroe", :class => "btn ")
+        text_field_tag(:hero_name)
+        text_area(:post, :description )
+        submit_tag("Add Hero", :class => "btn ")
       end
     end
 
