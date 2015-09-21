a8d7b88348b87c623ff3d0fe627c5cb9c1878f51
david <geldern@novastorm.de>
2015-09-21 17:06:05 +0200

    included js for ckedit

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -140,6 +140,7 @@ ActiveAdmin.setup do |config|
   #
   # To load a javascript file:
   #   config.register_javascript 'my_javascript.js'
+  config.register_javascript '//cdn.ckeditor.com/4.5.3/standard/ckeditor.js'
 
 
   # == CSV options
