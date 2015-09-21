10c7c6525cdc12ef3a1987155e1921727fe5bdae
david <geldern@novastorm.de>
2015-09-21 14:32:17 +0200

    fixed string error in app/admin/heroes.rb

--- app/admin/heroes.rb
+++ app/admin/heroes.rb
@@ -1,11 +1,11 @@
 ActiveAdmin.register_page "Helden" do
 
-  menu :priority => 1, :label => "Helden"
+  menu :label => "Helden"
 
-  content :title => Helden do
+  content :title => "Helden" do
     div :class => "blank_slate_container", :id => "dashboard_default_message" do
       span :class => "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
+        span 
         small I18n.t("active_admin.dashboard_welcome.call_to_action")
       end
     end
