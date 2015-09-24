609f01a6c58334bb3a6be927b4bc667495cf1c65
david <geldern@novastorm.de>
2015-09-24 14:46:44 +0200

    modified save function to hero controller

--- app/admin/heroes.rb
+++ app/admin/heroes.rb
@@ -3,13 +3,17 @@ ActiveAdmin.register_page "Helden" do
   controller do
     def index
       params[:hero1] = Herodata.where(name: 'img1').first
-      params[:hero2] = Herodata.where(name: 'img2').first
+      if params[:hero2].present?
+        params[:hero2].value = params[:hero2]
+      else
+        params[:hero2] = Herodata.where(name: 'img2').first
+      end
       params[:hero3] = Herodata.where(name: 'img3').first
       params[:herotext1] = Herodata.where(name: 'text1').first
       params[:herotext2] = Herodata.where(name: 'text2').first
     end
 
-    def create
+    def post
       hero1 = Herodata.where(name: 'img1').first
       hero1.value = params[:hero1]
       hero1.save
