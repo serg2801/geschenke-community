4092a9b6c92f0ae315559e93150dfa4d2d759bfc
david <geldern@novastorm.de>
2015-09-24 15:23:49 +0200

    used association and save for all values in hero controller

--- app/admin/heroes.rb
+++ app/admin/heroes.rb
@@ -2,22 +2,44 @@ ActiveAdmin.register_page "Helden" do
 
   controller do
     def index
-      params[:hero1] = Herodata.where(name: 'img1').first
+      if params[:hero1].present?
+         hero1 = Herodata.where(name: 'img1').first
+         hero1.value = params[:hero1]
+         hero1.save
+      else
+         params[:hero1] = Herodata.where(name: 'img1').first.value
+      end
       if params[:hero2].present?
-        params[:hero2] = params[:hero2]
+         hero2 = Herodata.where(name: 'img2').first
+         hero2.value = params[:hero2]
+         hero2.save
+         params[:hero2] = params[:hero2]
       else
         params[:hero2] = Herodata.where(name: 'img2').first.value
       end
-      params[:hero3] = Herodata.where(name: 'img3').first
-      params[:herotext1] = Herodata.where(name: 'text1').first
-      params[:herotext2] = Herodata.where(name: 'text2').first
+      if params[:hero3].present?
+         hero3 = Herodata.where(name: 'img3').first
+         hero3.value = params[:hero3]
+         hero3.save
+      else
+         params[:hero3] = Herodata.where(name: 'img3').first.value
+      end
+      if params[:herotext1].present?
+         hero1 = Herodata.where(name: 'text1').first
+         hero1.value = params[:herotext1]
+         hero1.save
+      else
+         params[:herotext1] = Herodata.where(name: 'text1').first.value
+      end
+      if params[:herotext2].present?
+         herotext2 = Herodata.where(name: 'text2').first
+         herotext2.value = params[:herotext2]
+         herotext2.save
+      else
+         params[:herotext2] = Herodata.where(name: 'text2').first.value
+      end
     end
 
-    def post
-      hero1 = Herodata.where(name: 'img1').first
-      hero1.value = params[:hero1]
-      hero1.save
-    end
   end
 
   menu :label => "Helden"
@@ -27,14 +49,5 @@ ActiveAdmin.register_page "Helden" do
       render :partial => 'form'
     end
   end # content
-
-  #page_action :update, method: :post do
-  ## ...
-  #  redirect_to 'admin/helden', notice: "Your event was added"
-  #end
-
-  #action_item :update do
-  #  link_to "Update", admin_helden_update_path, method: :post
-  #end
 
 end
